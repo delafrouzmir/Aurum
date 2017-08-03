af9ee0ca864079251d7621128bdd4c098cedc344
delafouzmir <delafrooz.mir16@gmail.com>
2017-08-03 02:13:05 -0400

    added error handling for browser cannot load users location

--- js/map.js
+++ js/map.js
@@ -1,27 +1,51 @@
+var map, mapCenter, mapOptions, infoWindow, mapZoom = 10;
+
+// Drawing the map
 function myMap() {
 
-	// The default center of the map is New York City
-	var mapCenter = {lat: 40.7128, lng: -74.0059};
+	//The default center of the map is NYC
+	mapCenter = {
+		lat : 40.7128,
+		lng : -74.0059
+	};
 
-	var mapOptions = {
-    center: mapCenter,
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
+	mapOptions = {
+		center : mapCenter,
+		zoom : mapZoom,
+		mapTypeId: google.maps.MapTypeId.ROADMAP 
+	};
+
+	map = new google.maps.Map(document.getElementById('mapOfJobs'), mapOptions);
 	
-	// If the browser supports geolocation,
-	// the center of the map would be the user's location
+	infoWindow = new google.maps.InfoWindow;
+
+	// Get the user's location if browser's geolocation works
+	// The map will be loaded around the user's location then
 	if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            mapCenter = {
-            	lat: position.coords.latitude,
-            	lng: position.coords.longitude
-            };
-            map.setCenter(mapCenter);
-        }
-        );
-	}
+		navigator.geolocation.getCurrentPosition(function(position) {
+    		var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
 
-	var map = new google.maps.Map(document.getElementById("mapOfJobs"), mapOptions);
-	
+			infoWindow.setPosition(pos);
+			infoWindow.setContent('Location found.');
+			infoWindow.open(map);
+			map.setCenter(pos);
+		}, function() {
+			handleLocationError(true, infoWindow, map.getCenter());
+		});
+	}
+	else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+	}
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+                    	'Error: Cannot load your location.' :
+                    	'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.open(map);
 }
